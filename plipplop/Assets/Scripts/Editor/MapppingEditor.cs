6a73176b1fbe5ebd8a9fba35447a43192eca2192
rackover <rackover@racknet.noip.me>
2019-10-09 16:48:49 +0200

    Cleaned mapping editor

--- plipplop/Assets/Scripts/Editor/MapppingEditor.cs
+++ plipplop/Assets/Scripts/Editor/MapppingEditor.cs
@@ -53,67 +53,6 @@ public class MappingEditor : Editor
             EditorGUILayout.LabelField("x", GUILayout.Width(width * 0.015f));
             EditorGUILayout.PropertyField(factor, GUIContent.none, GUILayout.Width(width * 0.05f));
 
-            /*
-            // Display the property fields in two ways.
-
-            if (DisplayFieldType == 0) {// Choose to display automatic or custom field types. This is only for example to help display automatic and custom fields.
-                //1. Automatic, No customization <-- Choose me I'm automatic and easy to setup
-                EditorGUILayout.LabelField("Automatic Field By Property Type");
-                EditorGUILayout.PropertyField(MyGO);
-                EditorGUILayout.PropertyField(MyInt);
-                EditorGUILayout.PropertyField(MyFloat);
-                EditorGUILayout.PropertyField(MyVect3);
-
-                // Array fields with remove at index
-                EditorGUILayout.Space();
-                EditorGUILayout.Space();
-                EditorGUILayout.LabelField("Array Fields");
-
-                if (GUILayout.Button("Add New Index", GUILayout.MaxWidth(130), GUILayout.MaxHeight(20))) {
-                    MyArray.InsertArrayElementAtIndex(MyArray.arraySize);
-                    MyArray.GetArrayElementAtIndex(MyArray.arraySize - 1).intValue = 0;
-                }
-
-                for (int a = 0; a < MyArray.arraySize; a++) {
-                    EditorGUILayout.PropertyField(MyArray.GetArrayElementAtIndex(a));
-                    if (GUILayout.Button("Remove  (" + a.ToString() + ")", GUILayout.MaxWidth(100), GUILayout.MaxHeight(15))) {
-                        MyArray.DeleteArrayElementAtIndex(a);
-                    }
-                }
-            }
-            else {
-                //Or
-
-                //2 : Full custom GUI Layout <-- Choose me I can be fully customized with GUI options.
-                EditorGUILayout.LabelField("Customizable Field With GUI");
-                MyGO.objectReferenceValue = EditorGUILayout.ObjectField("My Custom Go", MyGO.objectReferenceValue, typeof(GameObject), true);
-                MyInt.intValue = EditorGUILayout.IntField("My Custom Int", MyInt.intValue);
-                MyFloat.floatValue = EditorGUILayout.FloatField("My Custom Float", MyFloat.floatValue);
-                MyVect3.vector3Value = EditorGUILayout.Vector3Field("My Custom Vector 3", MyVect3.vector3Value);
-
-
-                // Array fields with remove at index
-                EditorGUILayout.Space();
-                EditorGUILayout.Space();
-                EditorGUILayout.LabelField("Array Fields");
-
-                if (GUILayout.Button("Add New Index", GUILayout.MaxWidth(130), GUILayout.MaxHeight(20))) {
-                    MyArray.InsertArrayElementAtIndex(MyArray.arraySize);
-                    MyArray.GetArrayElementAtIndex(MyArray.arraySize - 1).intValue = 0;
-                }
-
-                for (int a = 0; a < MyArray.arraySize; a++) {
-                    EditorGUILayout.BeginHorizontal();
-                    EditorGUILayout.LabelField("My Custom Int (" + a.ToString() + ")", GUILayout.MaxWidth(120));
-                    MyArray.GetArrayElementAtIndex(a).intValue = EditorGUILayout.IntField("", MyArray.GetArrayElementAtIndex(a).intValue, GUILayout.MaxWidth(100));
-                    if (GUILayout.Button("-", GUILayout.MaxWidth(15), GUILayout.MaxHeight(15))) {
-                        MyArray.DeleteArrayElementAtIndex(a);
-                    }
-                    EditorGUILayout.EndHorizontal();
-                }
-            }
-
-        */
             EditorGUILayout.Space();
 
             //Remove this index from the List
@@ -130,29 +69,4 @@ public class MappingEditor : Editor
         targetList.ApplyModifiedProperties();
     }
 
-
-    /*
-    public override void OnInspectorGUI()
-    {
-        DrawDefaultInspector();
-
-        var style = new GUIStyle();
-        style.fixedWidth = 1000;
-        style.fixedHeight = 300;
-
-        EditorGUILayout.Space();
-
-        EditorGUILayout.BeginHorizontal();
-        if (GUILayout.Button("+", EditorStyles.miniButton)) size++;
-        if (GUILayout.Button("-", EditorStyles.miniButton)) size--;
-        EditorGUILayout.EndHorizontal();
-
-        for (var i = 0; i < size; i++) {
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.EnumPopup(ACTION.ACTION);
-            EditorGUILayout.EnumPopup(ACTION.ACTION);
-            EditorGUILayout.EndHorizontal();
-        }
-    }
-    */
 }
