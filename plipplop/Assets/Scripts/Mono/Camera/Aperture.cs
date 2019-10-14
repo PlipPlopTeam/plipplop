4f7db064c7ae3c2863bbea40b843c56b22e08fdd
Barkintosh <l.perdu@rubika-edu.com>
2019-10-14 16:52:16 +0200

    Fixing distance effect

--- plipplop/Assets/Scripts/Mono/Camera/Aperture.cs
+++ plipplop/Assets/Scripts/Mono/Camera/Aperture.cs
@@ -188,8 +188,8 @@ public class Aperture : MonoBehaviour
 
             // The Speed Enhancement effect
             float ratio = Mathf.Clamp(targetMovementVelocity / maxEffect, 0f, 1f);
-            fovOffset = fovRange.x + (fovRange.y - fovRange.x) * ratio * multiplier;
-            distanceOffset = distanceRange.x + (distanceRange.y - distanceRange.x) * ratio * multiplier;
+            fovOffset = (fovRange.x + (fovRange.y - fovRange.x) * ratio) * multiplier;
+            distanceOffset = (distanceRange.x + (distanceRange.y - distanceRange.x) * ratio) * multiplier;
             
             lastTargetPosition = target.position;
         }
