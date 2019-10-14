5f534ee12567f9be6760b8373a0bfd0ca9b4b255
Barkintosh <l.perdu@rubika-edu.com>
2019-10-14 16:52:33 +0200

    Adding basic NPC

---
+++ plipplop/Assets/NPC.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class NPC : MonoBehaviour
+{
+    public Sight sight;
+    public FocusLook look;
+    public NavMeshAgent agent;
+    public Animator animator;
+
+    Transform t;
+
+    void Update()
+    {
+        GameObject[] seens = sight.Scan();
+        foreach(GameObject go in seens)
+        {
+            Controller c = go.GetComponent<Controller>();
+            if(c != null) 
+            {
+                look.FocusOn(c.transform);
+                t = c.transform;
+                return;
+            }
+        }
+
+        t = null;
+    }
+
+    void OnDrawGizmos()
+    {
+        if(t != null)
+            Gizmos.DrawLine(transform.position + sight.offset, t.position);
+    }
+}
