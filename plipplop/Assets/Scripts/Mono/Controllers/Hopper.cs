a7c7fd8a23190b368c11eb9bb8fb8f86a2ed21bc
Barkintosh <l.perdu@rubika-edu.com>
2019-10-25 14:42:06 +0200

    Creating Hopper.cs controler with handle hopping movement type and recreate Television specific behavior in Television.cs

---
+++ plipplop/Assets/Scripts/Mono/Controllers/Hopper.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hopper : Controller
+{
+    [Header("HOPPER")]
+    [Space(5)]
+    [Header("Push")]
+    public float pushMovementForce = 10f;
+    public float pushCooldown = 2f;
+    public float pushThreshold = 0.5f;
+    public float pushJumpForce = 10f;
+    [Space(5)]
+    [Header("Others")]
+    public float jumpForce = 10f;
+    public float speedBeforeLegs;
+    public float rotationVelocityThreshold = 0.25f;
+    public float rotationLerpSpeed = 2f;
+    public float crouchCooldown = 2f;
+    public float drag = 1f;
+
+    float crouchTimer;
+    float pushTimer;
+    bool pushed;
+    bool jumped;
+
+    internal override void SpecificMove(Vector3 direction)
+    {
+        if(!pushed && IsGrounded() && direction.magnitude > pushThreshold)
+            Push(direction);
+    }
+
+    internal virtual void Push(Vector3 direction)
+    {
+        Vector3 v = (Game.i.aperture.Right() * direction.x + Game.i.aperture.Forward() * direction.z) * pushMovementForce;
+        v += Vector3.up * pushJumpForce;
+        rigidbody.AddForce(v * Time.deltaTime, ForceMode.Impulse);
+        pushed = true;
+        pushTimer = pushCooldown;
+    }
+
+    internal override void Update()
+    {
+        base.Update();
+
+        if(IsPossessed())
+        {
+            if(pushed)
+            {
+                pushTimer -= Time.deltaTime;
+                if(pushTimer <= 0f) pushed = false;
+            }
+
+            if(rigidbody.velocity.magnitude > rotationVelocityThreshold)
+            {
+                Vector3 moveDirection = rigidbody.velocity.normalized;
+                transform.forward = Vector3.Lerp(
+                    transform.forward,
+                    new Vector3(moveDirection.x, 0f, moveDirection.z),
+                    Time.deltaTime * rotationLerpSpeed);
+            }
+        }
+        
+        if(!AreLegsRetracted())
+        {
+            crouchTimer -= Time.deltaTime;
+            if(crouchTimer <= 0)
+            {
+                if(IsPossessed() && rigidbody.velocity.magnitude <= speedBeforeLegs)
+                    RetractLegs();
+            }
+        }
+        else
+        {
+            if(IsGrounded(0.5f))
+            {
+                if(jumped)
+                {
+                    jumped = false;
+                    OnJumpDown();
+                }
+            }
+            else
+            {
+                if(!jumped) 
+                {
+                    jumped = true;
+                    OnJumpUp();
+                }
+            }
+        }
+    }
+
+    internal virtual void OnJumpUp(){}
+    internal virtual void OnJumpDown(){}
+
+    internal override void SpecificJump()
+    {
+        if(IsGrounded())
+        {
+            ExtendLegs();
+            rigidbody.AddForce(Vector3.up * jumpForce * Time.deltaTime, ForceMode.Impulse);
+        }
+    }
+
+    internal override void OnLegsRetracted()
+    {
+        rigidbody.drag = drag;
+    }
+
+    internal override void OnLegsExtended()
+    {
+        crouchTimer = crouchCooldown;
+    }
+}
--- plipplop/Assets/Scripts/Mono/Controllers/Television.cs
+++ plipplop/Assets/Scripts/Mono/Controllers/Television.cs
@@ -3,74 +3,50 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class Television : Controller
+public class Television : Hopper
 {
-    [Header("Specific properties")]
-
-    [Header("Push")]
-    public float pushMovementForce = 10f;
-    public float pushCooldown = 2f;
-    public float pushThreshold = 0.5f;
-    public float pushJumpForce = 10f;
-    [Space(5)]
-    [Header("Others")]
-    public float jumpForce = 10f;
-    public float speedBeforeLegs;
-    public float rotationVelocityThreshold = 0.25f;
-    public float rotationLerpSpeed = 2f;
-    public float crouchCooldown = 2f;
-    public float drag = 1f;
+    [Header("TELEVISION")]
     [Space(5)]
     [Header("References")]
-    public Animation anim;
-    public ParticleSystem ps;
     public Valuable valuable;
     public Renderer screenRenderer;
     public GameObject fakeLight;
-
+    public Animation anim;
+    public ParticleSystem ps;
     [Space(5)]
     [Header("Screen")]
     public Texture standScreenTexture;
     public Texture crouchScreenTexture;
     public Texture idleScreenTexture;
 
-    float crouchTimer;
-    float pushTimer;
-    bool pushed;
-    bool jumped;
-
     public override void OnEject()
     {
         base.OnEject();
         screenRenderer.material.mainTexture = idleScreenTexture;
         fakeLight.SetActive(true);
-        // Code here
     }
 
-    public override void OnPossess()
+    internal override void Push(Vector3 direction)
     {
-        base.OnPossess();
-        // Code here
-    }
-
-    internal override void SpecificMove(Vector3 direction)
-    {
-        if(!pushed && direction.magnitude > pushThreshold)
-        {
-            Vector3 v = (Game.i.aperture.Right() * direction.x + Game.i.aperture.Forward() * direction.z) * pushMovementForce;
-            v += Vector3.up * pushJumpForce;
-            rigidbody.AddForce(v * Time.deltaTime, ForceMode.Impulse);
-            anim.Play();
-            pushed = true;
-            pushTimer = pushCooldown;
-        }
+        base.Push(direction);
+        anim.Play();
     }
 
     internal override void Start()
     {
         base.Start();
         screenRenderer.material = Instantiate(screenRenderer.material);
-        // Code here
+    }
+
+    internal override void OnJumpUp()
+    {
+        valuable.hidden = true;
+    }
+
+    internal override void OnJumpDown()
+    {
+        ps.Play();
+        valuable.hidden = false;
     }
 
     void OnDestroy() 
@@ -78,82 +54,19 @@ public class Television : Controller
         Destroy(screenRenderer.material);
     }
 
-    internal override void Update()
-    {
-        base.Update();
-
-        if(IsPossessed())
-        {
-            if(pushed)
-            {
-                pushTimer -= Time.deltaTime;
-                if(pushTimer <= 0f) pushed = false;
-            }
-
-            if(rigidbody.velocity.magnitude > rotationVelocityThreshold)
-            {
-                Vector3 moveDirection = rigidbody.velocity.normalized;
-                transform.forward = Vector3.Lerp(
-                    transform.forward,
-                    new Vector3(moveDirection.x, 0f, moveDirection.z),
-                    Time.deltaTime * rotationLerpSpeed);
-            }
-        }
-        
-        if(!AreLegsRetracted())
-        {
-            crouchTimer -= Time.deltaTime;
-            if(crouchTimer <= 0)
-            {
-                if(IsPossessed() && rigidbody.velocity.magnitude <= speedBeforeLegs)
-                    RetractLegs();
-            }
-        }
-        else
-        {
-            if(IsGrounded(0.5f))
-            {
-                if(jumped)
-                {
-                    jumped = false;
-                    ps.Play();
-                    valuable.hidden = true;
-                }
-            }
-            else
-            {
-                if(!jumped) 
-                {
-                    valuable.hidden = false;
-                    jumped = true;
-                }
-                
-            }
-        }
-    }
-
-    internal override void SpecificJump()
-    {
-        if(AreLegsRetracted())
-        {
-            ExtendLegs();
-            rigidbody.AddForce(Vector3.up * jumpForce * Time.deltaTime, ForceMode.Impulse);
-        }
-    }
-
     internal override void OnLegsRetracted()
     {
-        // Code here
+        base.OnLegsRetracted();
+        
         valuable.hidden = true;
         fakeLight.SetActive(true);
-        rigidbody.drag = drag;
         screenRenderer.material.mainTexture = crouchScreenTexture;
     }
 
     internal override void OnLegsExtended()
     {
-        // Code here
-        crouchTimer = crouchCooldown;
+        base.OnLegsExtended();
+
         valuable.hidden = false;
         screenRenderer.material.mainTexture = standScreenTexture;
         fakeLight.SetActive(false);
