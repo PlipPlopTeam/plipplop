9d3db5f155b1d75d1800ee8119f91b3c88686b4e
rackover <rackover@racknet.noip.me>
2019-10-25 18:42:05 +0200

    Added longboard

---
+++ plipplop/Assets/Scripts/Mono/Controllers/Longboard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Longboard : Car
+{
+    [Header("Sub-specific properties")]
+    public float jumpForce = 15f;
+    public float airRollAccumulatorSpeed = 2f;
+
+    float airRollAccumulator = 0f;
+
+    internal override void SpecificJump()
+    {
+        base.SpecificJump();
+        if (IsGrounded()) {
+            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Acceleration);
+        }
+    }
+
+    internal override void AirPitchAndRoll(Vector3 direction)
+    {
+        // Pitch and roll
+        rigidbody.AddTorque(transform.right * direction.z * Time.fixedDeltaTime * airPitchForce, ForceMode.Acceleration);
+        rigidbody.AddTorque(-transform.forward * direction.x * Time.fixedDeltaTime * airRollForce, ForceMode.Acceleration);
+    }
+}
