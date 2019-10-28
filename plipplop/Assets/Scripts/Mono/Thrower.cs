fc2d0362d86885572411353b6af8993e597f60a6
Barkintosh <l.perdu@rubika-edu.com>
2019-10-28 16:23:31 +0100

    Adding some scripts, updating scenes and prefabs

---
+++ plipplop/Assets/Scripts/Mono/Thrower.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Thrower : MonoBehaviour
+{
+    [Header("Referencies")]
+    public GameObject prefabToThrow;
+    public Transform gunEnd;
+
+    [Header("Settings")]
+    public float force;
+
+    private Projectile arrow;
+
+    public void Reload()
+    {
+        if(arrow == null)
+        {
+            arrow = Instantiate(prefabToThrow, gunEnd.position, gunEnd.rotation)
+            .GetComponent<Projectile>();
+            arrow.Stuck();
+        }
+    }
+
+    public void Shoot()
+    {
+        if(arrow != null)
+        {
+            arrow.Throw(gunEnd.forward, force);
+            arrow = null;
+        }
+    }
+
+    public void OnDestroy()
+    {
+        if(arrow != null) Destroy(arrow.gameObject);
+    }
+}
---
+++ plipplop/Assets/Shooter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : Controller
+{
+    internal override void OnLegsRetracted()
+    {}
+
+    internal override void OnLegsExtended()
+    {}
+}
