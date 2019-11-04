a07431d25478bca42cc17dd6c54e63f5695cc76d
Barkintosh <l.perdu@rubika-edu.com>
2019-11-04 10:25:37 +0100

    Starting to work on Activities for the npcs

---
+++ plipplop/Assets/Scripts/Activity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activity : MonoBehaviour
+{
+    public NonPlayableCharacter user;
+
+    public void Use(NonPlayableCharacter npc)
+    {
+        user = npc;
+        npc.agentMovement.Stop();
+    }
+}
