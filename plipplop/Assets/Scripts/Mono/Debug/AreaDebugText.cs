407f56a5670ac351e3bf63304a9f966c9a395ac2
rackover <rackover@racknet.noip.me>
2019-11-04 15:53:52 +0100

    Updated gym level

--- plipplop/Assets/Scripts/Mono/Debug/AreaDebugText.cs
+++ plipplop/Assets/Scripts/Mono/Debug/AreaDebugText.cs
@@ -7,12 +7,18 @@ using TMPro;
 
 public class AreaDebugText : MonoBehaviour
 {
+    public enum FACING { TOP, FRONT}
+
     public string text;
+    public FACING facing;
     TextMeshPro tmp;
 
     private void OnValidate()
     {
         tmp = GetComponentInChildren<TextMeshPro>();
         tmp.text = text;
+
+        tmp.transform.rotation = facing == FACING.TOP ? Quaternion.Euler(90f, 180f, 0f) : Quaternion.Euler(0f, 180f, 0f);
+        tmp.transform.localPosition = (Vector3.one - Vector3.right) * 0.01f;
     }
 }
