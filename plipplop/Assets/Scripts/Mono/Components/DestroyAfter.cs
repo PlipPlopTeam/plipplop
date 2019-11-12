5a27fd87cfaf718d785a42fb555ee32b81abf047
rackover <rackover@racknet.noip.me>
2019-11-12 17:20:12 +0100

    Added utility script DestroyAfter

---
+++ plipplop/Assets/Scripts/Mono/Components/DestroyAfter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfter : MonoBehaviour
+{
+    public float lifespan = 5f;
+
+    float createTime = 0f;
+
+
+    private void Awake()
+    {
+        createTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (createTime + lifespan < Time.time) {
+            DestroyImmediate(gameObject);
+        }
+    }
+}
