04f42a934650615ec4416e5ec5d99907f4fdd8d8
rackover <rackover@racknet.noip.me>
2019-11-14 18:34:07 +0100

    Fixed autocast error on Geometry CS

--- plipplop/Assets/Scripts/Geometry.cs
+++ plipplop/Assets/Scripts/Geometry.cs
@@ -11,7 +11,7 @@ public static class Geometry
 
     public static bool IsPointInTriangle(Vector2 p, Vector2 p0, Vector2 p1, Vector2 p2)
     {
-        var A = 1 / 2 * (-p1.y * p2.x + p0.y * (-p1.x + p2.x) + p0.x * (p1.y - p2.y) + p1.x * p2.y);
+        var A = 1f / 2f * (-p1.y * p2.x + p0.y * (-p1.x + p2.x) + p0.x * (p1.y - p2.y) + p1.x * p2.y);
         var sign = A < 0 ? -1 : 1;
         var s = (p0.y * p2.x - p0.x * p2.y + (p2.y - p0.y) * p.x + (p0.x - p2.x) * p.y) * sign;
         var t = (p0.x * p1.y - p0.y * p1.x + (p0.y - p1.y) * p.x + (p1.x - p0.x) * p.y) * sign;
