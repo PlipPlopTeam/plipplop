1a435ae2dfdda9abec005bbfd7eeaf1a0802ea42
rackover <rackover@racknet.noip.me>
2019-11-15 11:50:17 +0100

    Implemented partial persistence

--- plipplop/Assets/Scripts/Managers/ChunkLoader.cs
+++ plipplop/Assets/Scripts/Managers/ChunkLoader.cs
@@ -10,11 +10,18 @@ public class ChunkLoader
     List<string> loadingChunks = new List<string>();
     ChunkStreamingZone playerChunkZone;
 
+    List<ChunkProp> props = new List<ChunkProp>();
+
+    class ChunkProp
+    {
+        public GameObject prop;
+        public string chunkIdentifier;
+    }
+
     public ChunkLoader()
     {
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
-
     public void Update()
     {        
         var c = Game.i.player.GetCurrentController();
@@ -30,11 +37,17 @@ public class ChunkLoader
                 }
             }
             if (playerCZ != playerChunkZone) {
+                // Check what I have to load and unload
                 var loadedChunks = chunkZones.Values.ToList().Where(o => { return o.IsLoaded(); });
                 var neighbors = playerCZ.neighborhood;
-                var chunksToLoad = neighbors.Where(o=> { return !loadedChunks.Contains(o); });
-                var chunksToUnload = loadedChunks.Where(o => { return !neighbors.Contains(o); });
+                var chunksToLoad = neighbors.Where(o=> { return !loadedChunks.Contains(o); }).ToList();
+                var chunksToUnload = loadedChunks.Where(o => { return !neighbors.Contains(o); }).ToList();
 
+                // Always load player chunk
+                chunksToUnload.RemoveAll(o => o.identifier == playerCZ.identifier);
+                chunksToLoad.Add(playerCZ);
+
+                // Commit
                 foreach (var cz in chunksToLoad) {
                     Load(cz.identifier);
                 }
@@ -67,8 +80,29 @@ public class ChunkLoader
 
     void Unload(string identifier)
     {
-        if (chunkZones[identifier].IsLoaded()) {
+        var csz = chunkZones[identifier];
+        if (csz.IsLoaded()) {
+            foreach(var prop in csz.scene.Value.GetRootGameObjects()) {
+                if (!csz.GetComponent<Persistent>()) {
+                    if (csz.IsInsideChunk(prop.transform.position)) {
+                        Object.Destroy(prop);
+                        props.RemoveAll(o => o.prop == prop);
+                    }
+                    else {
+                        foreach(var id in chunkZones.Keys) {
+                            if (!chunkZones[id].IsLoaded())
+                                continue;
 
+                            var newChunk = chunkZones[id];
+                            if (newChunk.IsInsideChunk(prop.transform.position)) {
+                                SceneManager.MoveGameObjectToScene(prop, newChunk.scene.Value);
+                            }
+                        }
+                    }
+                }
+            }
+            SceneManager.UnloadSceneAsync(csz.scene.Value);
+            csz.scene = null;
         }
     }
 
@@ -85,6 +119,16 @@ public class ChunkLoader
         foreach(var id in chunkZones.Keys) {
             if (scene.name == chunkZones[id].chunk.name) {
                 chunkZones[id].scene = scene;
+
+                foreach(var prop in scene.GetRootGameObjects()) {
+                    if (props.FindAll(o=>o.prop == prop).Count > 0){
+                        Object.DestroyImmediate(prop); // Destroy clones (object is still here)
+                        continue;
+                    }
+                    props.Add(new ChunkProp() { chunkIdentifier = id, prop = prop });
+                }
+
+                break;
             }
         }
     }
