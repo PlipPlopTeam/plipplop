e9aea616bfa0e9b9af9ba0d06203bd2b04f9ff55
rackover <rackover@racknet.noip.me>
2019-11-18 11:08:56 +0100

    Created fadein fadeout SG

---
+++ plipplop/Assets/Scripts/Mono/Components/FadedApparition.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadedApparition : MonoBehaviour
+{
+    Renderer[] renderers;
+    float alpha = 0f;
+    float speed = 1f;
+
+    private void Awake()
+    {
+        renderers = GetComponentsInChildren<Renderer>();
+        UpdateColor();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    private void OnDisable()
+    {
+        alpha = 0f;
+    }
+
+    public void StartFadingOut()
+    {
+        StartCoroutine(FadeOut());
+    }
+
+    public void StartFadeIn()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    IEnumerator FadeIn()
+    {
+        while(alpha < 1f) {
+            alpha += Time.deltaTime * speed;
+            UpdateColor();
+            yield return null;
+        }
+        yield return true;
+    }
+
+    IEnumerator FadeOut()
+    {
+        while (alpha > 0f) {
+            alpha -= Time.deltaTime * speed;
+            UpdateColor();
+            yield return null;
+        }
+        yield return true;
+    }
+
+    void UpdateColor()
+    {
+        foreach (var renderer in renderers) {
+            renderer.material.SetFloat(Shader.PropertyToID("_FadeAmount"), 1f-alpha);
+        }
+    }
+}
