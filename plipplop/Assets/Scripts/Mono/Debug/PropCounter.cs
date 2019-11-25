3af419f1f12751138dbd979eb0656177c823770c
rackover <rackover@racknet.noip.me>
2019-11-25 11:52:10 +0100

    Added components for chunk loading optimization

---
+++ plipplop/Assets/Scripts/Mono/Debug/PropCounter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PropCounter : MonoBehaviour
+{
+    string str;
+    TextMeshProUGUI txt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txt = GetComponent<TextMeshProUGUI>();
+        str = txt.text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txt.text = string.Format(str, Game.i.chunkLoader.GetPropCount().ToString());
+    }
+}
---
+++ plipplop/Assets/Scripts/Mono/Volumes/PersistenceVolume.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistenceVolume : Volume
+{
+    public ChunkStreamingZone parentChunkStreamingZone;
+
+    public override void OnPlayerEnter(Controller player)
+    {
+       // throw new System.NotImplementedException();
+    }
+
+    public override void OnPlayerExit(Controller player)
+    {
+       // throw new System.NotImplementedException();
+    }
+
+    public override void OnObjectEnter(Collider g)
+    {
+        if (!Game.i.chunkLoader.IsPersistent(g.gameObject)) {
+            Game.i.chunkLoader.Store(g.gameObject);
+        }
+    }
+
+    public override void OnObjectExit(Collider g)
+    {
+        if (!Game.i.chunkLoader.IsPersistent(g.gameObject)) {
+            Game.i.chunkLoader.RemoveFromStorage(g.gameObject);
+        }
+    }
+
+
+}
