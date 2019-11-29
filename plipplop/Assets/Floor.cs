39e9d59ebb532b35a0fe57ee8ae91dd53c285a83
Barkintosh <l.perdu@rubika-edu.com>
2019-11-29 16:35:55 +0100

    🛒 Cleaning Folders

---
+++ plipplop/Assets/Floor.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+	public enum EMaterial { SAND, DIRT, CONCRETE, WOOD };
+
+	[Header("Settings")]
+	public EMaterial material;
+	[Range(0f, 1f)] public float adherence;
+}
