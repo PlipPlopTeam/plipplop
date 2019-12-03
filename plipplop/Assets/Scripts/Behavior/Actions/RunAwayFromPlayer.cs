2f444091bef3f63f2ee03dad4ac7b78fef8275f2
Barkintosh <l.perdu@rubika-edu.com>
2019-12-03 18:28:30 +0100

    Updating Behavior

---
+++ plipplop/Assets/Scripts/Behavior/Actions/RunAwayFromPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Behavior.NPC
+{
+	[CreateAssetMenu(menuName = "Behavior/Action/NonPlayableCharacter/RunAwayFromPlayer")]
+	public class RunAwayFromPlayer : AIAction
+    {
+		public Vector2 distanceRange;
+		public override void Execute(NonPlayableCharacter target)
+        {
+            NonPlayableCharacter npc = target;
+            if (npc != null && npc.player != null)
+			{
+				Vector3 direction = -(npc.transform.position - npc.player.transform.position).normalized;
+				Vector3 position = npc.transform.position + direction * Random.Range(distanceRange.x, distanceRange.y);
+				npc.agentMovement.GoThere(position);
+			}
+
+		}
+	}
+}
