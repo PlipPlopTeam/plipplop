16e6c06ddcce529725ea3ae2fd953bc39e42e00e
Barkintosh <l.perdu@rubika-edu.com>
2019-12-06 15:01:25 +0100

    Finishing basic Detection and carry back behavior

---
+++ plipplop/Assets/Scripts/Behavior/Actions/DebugLogString.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Behavior;
+
+namespace Behavior.NPC
+{
+	[CreateAssetMenu(menuName = "Behavior/Action/Log")]
+	public class DebugLogString : AIAction
+	{
+		public string log = "LOG";
+		public override void Execute(NonPlayableCharacter target)
+		{
+			Debug.Log(Time.time + " : " + log);
+		}
+	}
+}
