dbf3a8cc6668b0f8c42a570f42d25af3508e6c4b
rackover <rackover@racknet.noip.me>
2019-12-11 10:22:18 +0100

    Added build scripts

---
+++ plipplop/Assets/Editor/BuildPlayerWindows64.cs
@@ -0,0 +1,40 @@
+﻿
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+public class BuildPlayerWindows64 : MonoBehaviour
+{
+    static string buildLocation = @"D:\PROJETS\PLIPPLOP\plipplop\PLIP_PLOP_BUILD\Build.exe";
+
+    [MenuItem("Tools/Build diorama")]
+    public static void BuildDiorama()
+    {
+        Build(buildLocation, new[] { "Assets/Scenes/Debug/Diorama.unity" });
+    }
+
+    [MenuItem("Tools/Build world scene")]
+    public static void BuildWorldScene()
+    {
+        Build(buildLocation, new[] { "Assets/Scenes/Main.unity" });
+    }
+
+    static void Build(string location, params string[] scenes)
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/Debug/Diorama.unity" };
+        buildPlayerOptions.locationPathName = location;
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded) {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed) {
+            Debug.LogError("Build failed");
+        }
+    }
+}
