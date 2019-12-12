bea1f2f72107cb8b179b53b5aaee52ca8258b1a4
Barkintosh <l.perdu@rubika-edu.com>
2019-12-12 17:26:07 +0100

    > Fixing Venturing State > Adding Artwork Activity

---
+++ plipplop/Assets/Artwork.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Artwork : Activity
+{
+	[Header("ARTWORK")]
+	public Vector3 focusOffset;
+	public float radius = 2f;
+
+	public override void Enter(NonPlayableCharacter user)
+	{
+		base.Enter(user);
+
+		Vector3 pos = Geometry.GetRandomPointAround(radius) + transform.position;
+		user.agentMovement.Stop();
+		user.agentMovement.GoThere(pos);
+		user.agentMovement.ClearEvents();
+		user.agentMovement.onDestinationReached += () =>
+		{
+			user.transform.LookAt(transform.position);
+			user.look.FocusOn(transform.position + focusOffset);
+		};
+	}
+
+	public override void Exit(NonPlayableCharacter user)
+	{
+		user.look.LooseFocus();
+		base.Exit(user);
+	}
+
+#if UNITY_EDITOR
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = new Color32(255, 215, 0, 255);
+		UnityEditor.Handles.DrawWireDisc(transform.position, Vector3.up, radius);
+		UnityEditor.Handles.DrawWireCube(transform.position + focusOffset, Vector3.one * 0.25f);
+	}
+#endif
+}
