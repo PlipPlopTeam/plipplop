5e673e7c6b2bc807160563dccc35dff88deed6d2
Barkintosh <l.perdu@rubika-edu.com>
2019-12-12 17:48:16 +0100

    > Carpet is now also a Serviette (Activity)

---
+++ plipplop/Assets/Serviette.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+public class Serviette : Activity
+{
+	[Header("SERVIETTE")]
+	public float offset;
+
+	public override void Enter(NonPlayableCharacter user)
+	{
+		base.Enter(user);
+		user.transform.position = transform.position + transform.forward * offset;
+		user.transform.forward = -transform.forward;
+		user.agentMovement.Stop();
+		user.agentMovement.ClearEvents();
+		user.animator.SetBool("Tanning", true);
+		full = true;
+	}
+
+	public override void Exit(NonPlayableCharacter user)
+	{
+		user.animator.SetBool("Tanning", false);
+		full = false;
+		base.Exit(user);
+	}
+
+#if UNITY_EDITOR
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = new Color32(255, 215, 0, 255);
+		UnityEditor.Handles.DrawWireCube(transform.position + transform.forward * offset, Vector3.one * 0.25f);
+	}
+#endif
+}
