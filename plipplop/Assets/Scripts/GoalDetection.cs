eb1378a5dbcce30f5b1aa55ebbeb50f2f79b4438
Kinopio27 <36189095+Kinopio27@users.noreply.github.com>
2019-12-15 17:47:42 +0100

    Updated main scene (UI, trees, feedback when finishing)

---
+++ plipplop/Assets/Scripts/GoalDetection.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class GoalDetection : MonoBehaviour {
+    public GameObject textMeshPro;
+
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.name == "BeachBall") {
+            textMeshPro.GetComponent<TextMeshPro>().SetText("success!");
+        }
+    }
+}
