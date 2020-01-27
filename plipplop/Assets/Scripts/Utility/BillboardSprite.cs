b53ac766d1158515da867fc623eec4e5aedb0d5c
Dorian Beaugendre <TahitiP4ncake@users.noreply.github.com>
2020-01-27 18:26:09 +0100

    magic bernie sprite + billboard

---
+++ plipplop/Assets/Scripts/Utility/BillboardSprite.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardSprite : MonoBehaviour
+{
+    private Transform target;
+
+    void Start()
+    {
+
+        if (Game.i)
+        {
+            target = Game.i.aperture.currentCamera.transform;
+        }else
+        {
+            target = Camera.main.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.forward = -(transform.position - target.position).normalized;
+    }
+}
