85d030f3ba49f276c02e5f74a9f6c8ca03806dcf
Barkintosh <l.perdu@rubika-edu.com>
2020-02-12 11:48:21 +0100

    > Improving Fishing System > Improving Activity Gizmo

--- plipplop/Assets/Scripts/FishingPole.cs
+++ plipplop/Assets/Scripts/FishingPole.cs
@@ -3,25 +3,26 @@
 public class FishingPole : Item
 {
 	public Transform poleEnd;
+	public Transform poleBentEnd;
 	public Transform plug;
 	public LineRenderer line;
+	public SkinnedMeshRenderer skinnedMeshRenderer;
+
+	public float plugOffset;
 
 	bool isBeingUsed;
 
 	public void Use()
 	{
 		isBeingUsed = true;
-		line.enabled = false;
-		plug.gameObject.SetActive(true);
+		UnPlunge();
 	}
 
 	public override void Drop()
 	{
 		base.Drop();
 		isBeingUsed = false;
-		line.enabled = false;
-		plug.gameObject.SetActive(false);
-		plug.SetParent(transform);
+		UnPlunge();
 	}
 
 	public void Plunge(Vector3 position)
@@ -29,14 +30,30 @@ public class FishingPole : Item
 		line.enabled = true;
 		plug.parent = null;
 		plug.position = position;
+		skinnedMeshRenderer.SetBlendShapeWeight(0, 100f);
+	}
+
+	public void UnPlunge()
+	{
+		line.enabled = false;
+		plug.SetParent(transform);
+		plug.localPosition = poleEnd.localPosition;
+		skinnedMeshRenderer.SetBlendShapeWeight(0, 0f);
 	}
 
 	public void Update()
 	{
 		if(isBeingUsed)
 		{
-			line.SetPositions(new Vector3[] { poleEnd.position, plug.position });
-			transform.up = Vector3.up;
+			line.SetPositions(new Vector3[] { poleBentEnd.position, plug.position });
+
+
+			plug.up = Vector3.up;
+			plugOffset = (Mathf.Sin(Time.time) / 1000f) * 0.5f;
+			plug.position = new Vector3(plug.position.x, plug.position.y + plugOffset, plug.position.z);
+
+			Vector3 dir = (transform.position - plug.position).normalized;
+			transform.right = dir;
 		}
 	}
 }
