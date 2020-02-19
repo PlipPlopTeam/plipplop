d7f05b4c0041606f256595cd43d3a489dd9001ea
Barkintosh <l.perdu@rubika-edu.com>
2020-02-19 15:19:45 +0100

    Improving NPC Behavior

---
+++ plipplop/Assets/Scripts/Behavior/Actions/RememberPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+namespace Behavior.NPC
+{
+	[CreateAssetMenu(menuName = "Behavior/Action/NonPlayableCharacter/RememberPlayer")]
+	public class RememberPlayer : AIAction
+	{
+		public float time = 5f;
+		public override void Execute(NonPlayableCharacter target)
+		{
+			NonPlayableCharacter npc = target;
+			if (npc != null) npc.RememberPlayerFor(time);
+		}
+	}
+}
+
