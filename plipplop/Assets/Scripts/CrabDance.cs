49878aec544bf60d315209d46a7b0929f7a82f19
Dorian Beaugendre <TahitiP4ncake@users.noreply.github.com>
2020-03-02 15:10:05 +0100

    crabRave 01

---
+++ plipplop/Assets/Scripts/CrabDance.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrabDance : MonoBehaviour
+{
+    public List<Crab> crabs;
+
+    public bool dancing;
+
+    public void StartDancing()
+    {
+        if (!dancing)
+        {
+            dancing = true;
+            StartCoroutine(DanceDelay());
+            SoundPlayer.Play("bgm_crab_rave", 1, 1, true);
+        }
+    }
+
+    IEnumerator DanceDelay()
+    {
+        foreach (var _crab in crabs)
+        {
+            _crab.gameObject.SetActive(true);
+            _crab.Pop();
+            
+            yield return new WaitForSecondsRealtime(.1f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Radio _radio = other.gameObject.GetComponent<Radio>();
+
+        if (_radio)
+        {
+            if (_radio.IsRadioOn())
+            {
+                StartDancing();
+            }
+        }
+    }
+}
