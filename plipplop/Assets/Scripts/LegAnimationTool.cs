0f9cf1bd438c28cc0c40875a32483acf60cdc44c
Dorian Beaugendre <TahitiP4ncake@users.noreply.github.com>
2020-03-18 17:40:24 +0100

    plip plop animations scenes

---
+++ plipplop/Assets/Scripts/LegAnimationTool.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegAnimationTool : MonoBehaviour
+{
+    
+    public LegAnimator legs;
+
+    public int animationIndex;
+
+    public Transform visuals;
+
+
+    void Start()
+    {
+        legs.Play(legs.currentAnimation.animationName);
+        
+        legs.Attach(visuals);
+
+        
+        Game.i.player.Eject();
+        Destroy(FindObjectOfType<BaseController>().gameObject);
+    }
+
+  
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            NextAnimation();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            PreviousAnimation();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ReplayAnimation();
+        }
+    }
+
+    void NextAnimation()
+    {
+        animationIndex++;
+
+        if (animationIndex >= legs._animations.Count)
+        {
+            animationIndex = 0;
+        }
+        
+        legs.Play(legs._animations[animationIndex].animationName);
+    }
+
+    void PreviousAnimation()
+    {
+        animationIndex--;
+        
+        if (animationIndex <0)
+        {
+            animationIndex = legs._animations.Count-1;
+        }
+        legs.Play(legs._animations[animationIndex].animationName);
+    }
+
+    void ReplayAnimation()
+    {
+        legs.Play(legs.currentAnimation.animationName);
+    }
+}
