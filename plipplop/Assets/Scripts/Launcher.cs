41c901cf0d31733ae277da8fb89aaf18f81802c6
Barkintosh <l.perdu@rubika-edu.com>
2020-03-26 17:31:56 +0100

    Improving Launcher System > Adding Delat, GFX, Animation > Debbuging

--- plipplop/Assets/Scripts/Launcher.cs
+++ plipplop/Assets/Scripts/Launcher.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using Cinemachine;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 public class Launcher : MonoBehaviour
 {
@@ -10,12 +12,25 @@ public class Launcher : MonoBehaviour
 	[Header("Settings")]
 	public AnimationCurve speedCurve;
 	public float speedMultiplier = 1f;
+	public float delay = 0f;
+	public string throwGameEffect = "";
+	public string releaseGameEffect = "";
+	public bool alignWithVelocity = true;
+	public float rotationSpeed = 0f;
+	[Header("Camera")]
+	public Transform aim;
+	public float FOV = 60f;
+	public bool isImmediate = false;
+	public bool lookAtTarget = false;
 
 	public Action<GameObject> onArrived;
 
 	private bool throwing = false;
 	private GameObject thrownObject;
 	private float progression = 0.5f;
+	private int lookAtIndex = 0;
+	private Geometry.PositionAndRotation objective;
+	private Vector3 lastPosition;
 
 	public void Update()
 	{
@@ -32,23 +47,107 @@ public class Launcher : MonoBehaviour
 	public void Frame()
 	{
 		cart.m_Position = progression * (path.m_Waypoints.Length - 1);
+
+		if (alignWithVelocity && thrownObject != null)
+		{
+			thrownObject.transform.Rotate(Vector3.up * rotationSpeed);
+		}
+		else
+		{
+			thrownObject.transform.up = Vector3.up;
+		}
+
+		lastPosition = cart.gameObject.transform.position;
+	}
+
+	public void PrepareLaunch(GameObject obj, Action<GameObject> onArrivedEvent = null)
+	{
+		if (delay > 0) StartCoroutine(WaitAndLaunch(delay, obj, onArrivedEvent));
+		else
+		{
+			PlaceCamera();
+			Launch(obj, onArrivedEvent);
+		}
+	}
+
+	IEnumerator WaitAndLaunch(float time, GameObject obj, Action<GameObject> onArrivedEvent = null)
+	{
+		PlaceCamera();
+		yield return new WaitForSeconds(time);
+		Launch(obj, onArrivedEvent);
+	}
+
+	public void PlaceCamera()
+	{
+		if (isImmediate)
+		{
+			//Game.i.aperture.Freeze();
+			Game.i.aperture.AddStaticPosition(aim.position, aim.rotation);
+			Game.i.aperture.FixedUpdate();
+			Game.i.aperture.fieldOfView.destination = FOV;
+			Game.i.aperture.Teleport();
+		}
+		else
+		{
+			objective = Game.i.aperture.AddStaticPosition(aim);
+		}
+
+		if (!lookAtTarget)
+		{
+			lookAtIndex = Game.i.aperture.DisableLookAt();
+		}
+		else
+		{
+			Game.i.aperture.EnableLookAt();
+		}
+	}
+
+	public Vector3 GetThrowDirection()
+	{
+		if (path.m_Waypoints.Length < 2) return Vector3.up;
+		return (path.m_Waypoints[1].position - path.m_Waypoints[0].position).normalized;
+	}
+
+	public void ResetCamera()
+	{
+		if (isImmediate)
+		{
+			Game.i.aperture.Unfreeze();
+		}
+		else
+		{
+			Game.i.aperture.RemoveStaticPosition(objective);
+		}
+		if (!lookAtTarget)
+		{
+			Game.i.aperture.RestoreLookAt(lookAtIndex);
+		}
+
+		if (releaseGameEffect != "")
+		{
+			Pyromancer.PlayGameEffect(releaseGameEffect, thrownObject.transform.position);
+		}
 	}
 
 	public void Launch(GameObject obj, Action<GameObject> onArrivedEvent = null)
 	{
+		if (obj == thrownObject) return;
+
 		thrownObject = obj;
 		throwing = true;
 		progression = 0f;
 		thrownObject.transform.SetParent(cart.transform);
 		thrownObject.transform.localPosition = Vector3.zero;
 		thrownObject.transform.localEulerAngles = Vector3.zero;
+		thrownObject.transform.up = -cart.gameObject.transform.forward;
 
-		if (onArrivedEvent != null) onArrived += onArrivedEvent;
+		if (onArrivedEvent != null) onArrived += onArrivedEvent;	
+		if (throwGameEffect != "") Pyromancer.PlayGameEffect(throwGameEffect, thrownObject.transform.position);
 	}
 
 	public void Release()
 	{
-		Debug.Log("Released");
+		ResetCamera();
 		if (onArrived != null)
 		{
 			onArrived.Invoke(thrownObject);
@@ -63,17 +162,18 @@ public class Launcher : MonoBehaviour
 	{
 		if(other.gameObject.TryGetComponent<BaseController>(out BaseController controller))
 		{
-			LaunchController(controller);
+			if (thrownObject != controller.gameObject) LaunchController(controller);
 		}
 	}
-
-	public void LaunchController(Controller cont)
+	public void LaunchController(Controller controller)
 	{
-		cont.Paralyse();
-		cont.Freeze();
-		Launch(cont.gameObject, (gameObject) => {
-			cont.UnParalyse();
-			cont.UnFreeze();
+		controller.Paralyse();
+		controller.Freeze();
+		controller.locomotion.StartFly();
+		PrepareLaunch(controller.gameObject, (go) => {
+			controller.UnParalyse();
+			controller.UnFreeze();
+			controller.locomotion.EndFly();
 		});
 	}
 }
