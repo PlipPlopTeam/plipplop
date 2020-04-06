861a519831fe4475980587701923b5892a45bbb5
rackover <rackover@racknet.noip.me>
2020-04-06 17:20:34 +0200

    Added dialog prompt

---
+++ plipplop/Assets/Scripts/Mono/Components/DialogPrompt.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogPrompt : MonoBehaviour
+{
+    public float markerHeight = 2f;
+    public Image promptLogo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        promptLogo.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Game.i.player.currentChatOpportunity != null && !Game.i.player.IsParalyzed()) {
+            promptLogo.enabled = true;
+            promptLogo.rectTransform.position = Camera.main.WorldToScreenPoint(Game.i.player.currentChatOpportunity.transform.position + Vector3.up*markerHeight);
+        }
+        else {
+            promptLogo.enabled = false;
+        }
+    }
+}
