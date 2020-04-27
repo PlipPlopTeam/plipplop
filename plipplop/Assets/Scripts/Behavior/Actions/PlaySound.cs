fdbd9f33df4f97d8575acff1dc557385e398a058
Barkintosh <l.perdu@rubika-edu.com>
2020-04-27 15:41:44 +0200

    Implementing new play sound action

---
+++ plipplop/Assets/Scripts/Behavior/Actions/PlaySound.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Behavior.NPC
+{
+    [CreateAssetMenu(menuName = "Behavior/Action/NonPlayableCharacter/PlaySound")]
+    public class PlaySound : AIAction
+    {
+        public string sname = "";
+        public float volume = 1f;
+        public float pitch = 1f;
+        public bool playAtFeet = true;
+        public bool fadeIn = false;
+        public override void Execute(NonPlayableCharacter target)
+        {
+            NonPlayableCharacter npc = target;
+            if (npc != null && sname != "")
+            {
+                if(playAtFeet)
+                    SoundPlayer.PlayAtPosition(sname, npc.transform.position, volume, false, fadeIn);
+                else
+                    SoundPlayer.Play(sname, volume, pitch, fadeIn);
+            }
+        }
+    }
+}
