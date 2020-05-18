508b2a6d5dc117303500253290fd32ab500c41c6
Dorian Beaugendre <TahitiP4ncake@users.noreply.github.com>
2020-05-18 16:47:13 +0200

    buildings v1

---
+++ plipplop/Assets/Scripts/Pause.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public bool paused;
+
+    public GameObject pauseMenu;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused)
+            {
+                StopPause();
+
+            }
+            else
+            {
+                StartPause();
+
+            }
+
+        }
+    }
+
+
+
+    void StartPause()
+    {
+        Time.timeScale = 0;
+        Time.fixedDeltaTime = .02f * Time.timeScale;
+        paused = true;
+        
+        pauseMenu.SetActive(true);
+    }
+
+    void StopPause()
+    {
+        Time.timeScale = 1;
+        Time.fixedDeltaTime = .02f * Time.timeScale;
+        paused = false;
+        pauseMenu.SetActive(false);
+    }
+}
