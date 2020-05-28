b5854e36e2e58b9f02ef915352d5bac8999b5496
Barkintosh <l.perdu@rubika-edu.com>
2020-05-28 16:25:08 +0200

    Making TV, Rabio and Pot Vanilla (Rotative)

--- plipplop/Assets/Scripts/Mono/Controllers/Pot.cs
+++ plipplop/Assets/Scripts/Mono/Controllers/Pot.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Pot : Hopper
+public class Pot : Vanilla
 {
-
 }
--- plipplop/Assets/Scripts/Mono/Controllers/Vanilla.cs
+++ plipplop/Assets/Scripts/Mono/Controllers/Vanilla.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class Vanilla : Controller
 {
-    internal override void OnLegsExtended() {}
+	[Header("Settings")]
+	public float angularForce = 2000f;
+	public float maxVelocityMagnitude = 1f;
+
+	internal override void OnLegsExtended() {}
     internal override void OnLegsRetracted() {}
+
+	internal override void SpecificMove(Vector3 direction)
+	{
+		Vector3 d = (Game.i.aperture.Forward() * -direction.x + Game.i.aperture.Right() * direction.z);
+		rigidbody.angularVelocity += d * angularForce;
+		rigidbody.angularVelocity = Vector3.ClampMagnitude(rigidbody.angularVelocity, maxVelocityMagnitude);
+	}
 }
