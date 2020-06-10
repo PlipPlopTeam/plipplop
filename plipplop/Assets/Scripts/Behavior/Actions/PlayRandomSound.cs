c1f9ea7de9282a7f75deab66937ab180e23961b9
Barkintosh <l.perdu@rubika-edu.com>
2020-06-10 12:02:52 +0200

    Adding Random Fear Sounds

---
+++ plipplop/Assets/Scripts/Behavior/Actions/PlayRandomSound.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Behavior.NPC
+{
+    [CreateAssetMenu(menuName = "Behavior/Action/NonPlayableCharacter/PlayRandomSound")]
+    public class PlayRandomSound : AIAction
+    {
+        public string[] sounds;
+        public float volume = 1f;
+        public float pitch = 1f;
+        public bool playAtFeet = true;
+        public bool fadeIn = false;
+        public override void Execute(NonPlayableCharacter target)
+        {
+            NonPlayableCharacter npc = target;
+            if (npc != null && sounds.Length > 0)
+            {
+				string s = sounds[Random.Range(0, sounds.Length)];
+
+                if(playAtFeet)
+                    SoundPlayer.PlayAtPosition(s, npc.transform.position, volume, false, fadeIn);
+                else
+                    SoundPlayer.Play(s, volume, pitch, fadeIn);
+            }
+        }
+    }
+}
