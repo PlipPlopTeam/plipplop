4c9627863e74a67bc25df233d0ef30ff22cbe263
Barkintosh <l.perdu@rubika-edu.com>
2020-06-19 11:28:57 +0200

    Fixing things about npc, sights and activities

--- plipplop/Assets/Scripts/Mono/Behaviors/Sight.cs
+++ plipplop/Assets/Scripts/Mono/Behaviors/Sight.cs
@@ -12,11 +12,17 @@ public class Sight : MonoBehaviour
 		public float range = 5f;
 	}
 	public Settings settings;
+	public Transform origin;
 	[Range(0f, 1f)] public float multiplier = 1f;
-	const float refresh = 0.5f;
+	const float refresh = 1f;
 
 
 	public Dictionary<string, float> memory = new Dictionary<string, float>();
+
+	public void Awake()
+	{
+		if (origin == null) origin = transform;
+	}
 
 	public GameObject[] Scan()
 	{
@@ -38,11 +44,11 @@ public class Sight : MonoBehaviour
 		else memory.Add(typeof(T).ToString(), refresh);
 
         List<T> objects = new List<T>();
-        Vector3 headPosition = transform.position + settings.offset;
+        Vector3 headPosition = origin.position + settings.offset;
 
         // Add object in range of being seen
         RaycastHit[] sphereHits;
-        sphereHits = Physics.SphereCastAll(headPosition, settings.range * multiplier, transform.forward, settings.range);
+        sphereHits = Physics.SphereCastAll(headPosition, settings.range * multiplier, origin.forward, settings.range);
         for(int i = 0; i < sphereHits.Length; i++)
         {
 			if(sphereHits[i].transform.gameObject.GetComponent<T>() != null && See(sphereHits[i].transform.gameObject))
@@ -55,17 +61,18 @@ public class Sight : MonoBehaviour
 
 	public bool See(GameObject o)
 	{
-		Vector3 headPosition = transform.position + settings.offset;
+		Vector3 headPosition = origin.position + settings.offset;
 		Vector3 direction = o.transform.position - headPosition;
-		float angle = Vector3.Angle(direction, transform.forward);
+		float angle = Vector3.Angle(direction, origin.forward);
 		if (angle < settings.fieldOfViewAngle * 0.5f * multiplier)
 		{
 			bool seen = true;
 			foreach (RaycastHit hit in Physics.RaycastAll(headPosition, direction, settings.range * multiplier, 5, QueryTriggerInteraction.Ignore))
 			{
-				if (hit.transform != transform && !o.transform.IsYourselfCheck(hit.transform))
+				if (hit.transform != origin && !o.transform.IsYourselfCheck(hit.transform))
 				{
-					if (Vector3.Distance(headPosition, o.transform.position) > Vector3.Distance(headPosition, hit.point)) seen = false;
+					//if (Vector3.Distance(headPosition, o.transform.position) > Vector3.Distance(headPosition, hit.point)) seen = false;
+					seen = false;
 				}
 			}
 
@@ -73,19 +80,22 @@ public class Sight : MonoBehaviour
 		}
 		else return false;
 	}
-/*
+
 #if UNITY_EDITOR
     void OnDrawGizmosSelected()
     {
+		Transform o = origin;
+		if (o == null) o = transform;
+
         Gizmos.color = new Color32(255, 215, 0, 255);
         UnityEditor.Handles.color = new Color32(255, 215, 0, 255);
-        Vector3 pos = transform.position + settings.offset;
-        Vector3 end = pos + transform.forward * settings.range;
+        Vector3 pos = o.position + settings.offset;
+        Vector3 end = pos + o.forward * settings.range;
         float radius = settings.range * Mathf.Tan((settings.fieldOfViewAngle * Mathf.Deg2Rad)/2f);
-        Gizmos.DrawLine(pos, pos + transform.forward * settings.range);
+        Gizmos.DrawLine(pos, pos + o.forward * settings.range);
         UnityEditor.Handles.DrawWireDisc(
             end,
-            (transform.position - (transform.position + transform.forward * settings.range)).normalized, 
+            (o.position - (o.position + o.forward * settings.range)).normalized, 
             radius
         );
     }
@@ -99,5 +109,5 @@ public class Sight : MonoBehaviour
         else if(settings.fieldOfViewAngle > 359f) settings.fieldOfViewAngle = 359f;
     }
 #endif
-*/
+
 }
